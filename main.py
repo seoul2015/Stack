82722ed6059d0635aacc1f9cdeed9540c514de36
seoul2015 <seoul2015.11.14@gmail.com>
2021-08-23 20:11:24 +0900

    수정 완료

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
 import sys
 
 class Stack:
-    Stack = []
-    Stack_size = 0
+    stack = []
+    stack_size = 0
     def push(self, num):
         self.stack_size += 1
         self.stack.append(num)
 
-    def pop(self, num):
-        if self.size > 0:
+    def pop(self):
+        if self.stack_size > 0:
             self.stack_size -= 1
-            return self.Stack.pop(self.Stack_size)
+            return self.stack.pop(self.stack_size)
+        else:
+            return -1
 
     def size(self):
-        return self.Stack_size
+        return self.stack_size
 
     def empty(self):
-        if self.Stack_size == 0:
+        if self.stack_size == 0:
             return 1
         else:
             return 0
 
     def top(self):
-        if self.Stack_size > 0:
-            return self.Stack[self.Stack_size-1]
-        else:
+        if self.stack_size == 0:
             return -1
+        else:
+            return self.stack[self.stack_size-1]
 
 i = int(input())
-Stack = Stack()
-while i>0:
+stack = Stack()
+while i > 0:
     i -= 1
     command = sys.stdin.readline().rstrip().split()
     if command[0] == 'push':
-        Stack.push(int(command[1]))
+        stack.push(int(command[1]))
     elif command[0] == 'top':
-        print(Stack.top)
+        print(stack.top())
     elif command[0] == 'size':
-        print(Stack.size())
+        print(stack.size())
     elif command[0] == 'empty':
-        print(Stack.size())
+        print(stack.empty())
     else:
-        print(Stack.pop())
+        print(stack.pop())
 
 
 
